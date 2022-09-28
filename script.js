0d459f2cd8bd8ecaed4916d9c490ff745b4660a3
Elisa <elisa.gest@hotmail.fr>
2022-09-28 16:30:38 +0200

    json local storage partie 2

--- script.js
+++ script.js
@@ -34,3 +34,36 @@ buttonHome.addEventListener("click", function() {
 localStorage.setItem("prenom","elisa");
 localStorage.getItem("prenom");
 
+let voit1 = {
+    voiture1 : "peugeot"
+}
+let car = JSON.stringify(voit1);
+localStorage.setItem("voiture",voit1);
+
+
+let voit2 = {
+    voiture2 : "citroen"
+}
+let car = JSON.stringify(voit2);
+localStorage.setItem("citroen",voit2);
+
+
+let voit3 = {
+    voiture3 : "audi"
+}
+let car = JSON.stringify(voit3);
+localStorage.setItem("audi",voit3);
+
+
+let voit4 = {
+    voiture4 : "bmw"
+}
+let car = JSON.stringify(voit4);
+localStorage.setItem("bmw",voit4);
+
+
+let voit5 = {
+    voiture5 : "peug"
+}
+let car = JSON.stringify(voit5);
+localStorage.setItem("peug",voit5);
