4b347fdb14511d6f5c4b37d4a90daf2b3d38e431
Elisa <elisa.gest@hotmail.fr>
2022-09-28 15:52:43 +0200

    compter les clicks

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let test = document.getElementById("container");
 test.appendChild(newTest); 
 
 
+
+
 const newTet = document.createElement("div");
 let tet = document.getElementById("container");
 tet.appendChild(newTet);
@@ -18,3 +20,12 @@ const newTete = document.createElement("div");
 tet.appendChild(newTet);
 newTete.innerHTML = ("<p>Elément 2</p>");
 console.log (myElement);
+
+
+
+function clickHandler(event) {
+    console.log('Bouton clické');
+}
+ 
+const btn = document.getElementById('.btn');
+btn.addEventListener('click', clickHandler);
