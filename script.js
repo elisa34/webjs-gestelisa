dd8ec779be9388258c89a1adb8626da986572902
Elisa <elisa.gest@hotmail.fr>
2022-09-28 14:30:35 +0200

    recup les 3 elements du conteneurs et de les loger (console log) partie 2 test

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const myElement = document.getElementsByTagName('div');
 console.log (myElement);
+
+const newTest = document.createElement("div");
+let test = document.getElementById("main");
+
+test.appendChild(newTest); 
