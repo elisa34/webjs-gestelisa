a0ba55b9607b74ca220e4fb9f0c1acc5e977f5f3
Elisa <elisa.gest@hotmail.fr>
2022-09-28 15:17:42 +0200

    recup les 3 elements du conteneurs et de les loger (console log) partie 3 finie

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const myElement = document.getElementById('container')
-console.log (myElement)
+
 
 const newTest = document.createElement("div");
 let test = document.getElementById("container");
 
 test.appendChild(newTest); 
 
+
+const newTet = document.createElement("div");
+let tet = document.getElementById("container");
+tet.appendChild(newTet);
+newTet.innerHTML = ("<p>Elément 1</p>");
+
+
+const newTete = document.createElement("div");
+
+tet.appendChild(newTet);
+newTete.innerHTML = ("<p>Elément 2</p>");
+console.log (myElement);
